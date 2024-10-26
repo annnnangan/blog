59e7e884783c3a0d26c9805a9ec76cb30126d2fd
anna <anna.ymngan@gmail.com>
2024-10-26 18:11:10 +0800

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,8 +31,8 @@ const oklchToHex = str => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://fuwari.vercel.app/',
-  base: '/',
+  site: 'https://annnnangan.github.io/',
+  base: '/blog',
   trailingSlash: 'always',
   integrations: [
     tailwind(),
